68fd8f6a80e535059cc0d44886bb7ec70885c183
N0I0C0K <nick131410@aliyun.com>
2026-04-04 16:52:48 +0800

    fix format

--- packages/i18n/lib/type.ts
+++ packages/i18n/lib/type.ts
@@ -7,6 +7,9 @@ import type deMessage from '../locales/de/messages.json'
 import type zh_CNMessage from '../locales/zh_CN/messages.json'
 import type zh_TWMessage from '../locales/zh_TW/messages.json'
 
-export type MessageKey = keyof typeof enMessage & keyof typeof deMessage & keyof typeof zh_CNMessage & keyof typeof zh_TWMessage
+export type MessageKey = keyof typeof enMessage &
+  keyof typeof deMessage &
+  keyof typeof zh_CNMessage &
+  keyof typeof zh_TWMessage
 
 export type DevLocale = 'en' | 'de' | 'zh_CN' | 'zh_TW'
