ba4128b3bcfc77e6af4b27eedced7d81e48bfa7d
N0I0C0K <nick131410@aliyun.com>
2026-04-05 17:10:20 +0800

    fix format

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -19,9 +19,9 @@ const manifest = {
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
   optional_host_permissions: [
-    'https://api.github.com/*',    // Version check API
-    'https://wallhaven.cc/*',      // Wallpaper search API
-    'wss://broker.emqx.io:8084/*',  // MQTT WebSocket connection
+    'https://api.github.com/*', // Version check API
+    'https://wallhaven.cc/*', // Wallpaper search API
+    'wss://broker.emqx.io:8084/*', // MQTT WebSocket connection
   ],
   permissions: ['storage', 'tabs', 'notifications', 'search', 'history', 'favicon', 'bookmarks', 'alarms', 'topSites'],
   // options_page: 'options/index.html',
